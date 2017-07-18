8cfdcb88c89288f0b544a08a5f9c471d4ef3d3f9
Denys <deno4ka@gmail.com>
2017-07-18 02:19:35 -0400

    added _14_Longest_Common_Prefix

--- src/com/deno4ka/Main.java
+++ src/com/deno4ka/Main.java
@@ -1,11 +1,12 @@
 package com.deno4ka;
 
+import com.deno4ka.LeetCode.Easy._14_Longest_Common_Prefix;
 import com.deno4ka.LeetCode.Easy._21_Merge_Two_Sorted_Lists;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My first GIT");
+        _14_Longest_Common_Prefix longestCommonPrefix = new _14_Longest_Common_Prefix();
         _21_Merge_Two_Sorted_Lists merge_two_sorted_lists = new _21_Merge_Two_Sorted_Lists();
     }
 }
