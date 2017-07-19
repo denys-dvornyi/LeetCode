df6e4f5175a6487d976b3f847d2bc43360fea930
Denys <deno4ka@gmail.com>
2017-07-19 06:49:55 -0400

    added _27_remove_element

--- src/com/deno4ka/Main.java
+++ src/com/deno4ka/Main.java
@@ -3,12 +3,14 @@ package com.deno4ka;
 import com.deno4ka.LeetCode.Easy._14_Longest_Common_Prefix;
 import com.deno4ka.LeetCode.Easy._21_Merge_Two_Sorted_Lists;
 import com.deno4ka.LeetCode.Easy._26_remove_duplicates_from_sorted_array;
+import com.deno4ka.LeetCode.Easy._27_remove_element;
 
 public class Main {
 
     public static void main(String[] args) {
 //        _14_Longest_Common_Prefix longestCommonPrefix = new _14_Longest_Common_Prefix();
 //        _21_Merge_Two_Sorted_Lists merge_two_sorted_lists = new _21_Merge_Two_Sorted_Lists();
-        _26_remove_duplicates_from_sorted_array remove_duplicates_from_sorted_array = new _26_remove_duplicates_from_sorted_array();
+//        _26_remove_duplicates_from_sorted_array remove_duplicates_from_sorted_array = new _26_remove_duplicates_from_sorted_array();
+        _27_remove_element remove_element = new _27_remove_element();
     }
 }
