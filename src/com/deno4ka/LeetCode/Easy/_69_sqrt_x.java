a4c119637417c3bf825a0fe77a671d6e00f5427a
Denys <deno4ka@gmail.com>
2017-08-11 11:14:10 -0400

    added _69_sqrt_x

---
+++ src/com/deno4ka/LeetCode/Easy/_69_sqrt_x.java
@@ -0,0 +1,16 @@
+package com.deno4ka.LeetCode.Easy;
+
+//Implement int sqrt(int x).
+//Compute and return the square root of x.
+
+public class _69_sqrt_x {
+
+    public _69_sqrt_x() {
+        System.out.println(mySqrt(0));
+    }
+
+    public int mySqrt(int x) {
+        return 0;
+    }
+
+}
--- src/com/deno4ka/Main.java
+++ src/com/deno4ka/Main.java
@@ -15,6 +15,7 @@ public class Main {
 //        _53_maximum_subarray maximum_subarray = new _53_maximum_subarray();
 //        _58_length_of_last_word length_of_last_word = new _58_length_of_last_word();
 //        _66_plus_one plus_one = new _66_plus_one();
-        _67_add_binary add_binary = new _67_add_binary();
+//        _67_add_binary add_binary = new _67_add_binary();
+        _69_sqrt_x sqrt_x = new _69_sqrt_x();
     }
 }
