3a018165eb3bfe878ebb544a9b75b507ddcf76ef
Denys <deno4ka@gmail.com>
2017-08-24 04:24:49 -0400

    solved: _100_same_tree

--- src/com/deno4ka/LeetCode/Easy/_100_same_tree.java
+++ src/com/deno4ka/LeetCode/Easy/_100_same_tree.java
@@ -6,13 +6,37 @@ package com.deno4ka.LeetCode.Easy;
 public class _100_same_tree {
 
     public _100_same_tree() {
-        TreeNode p = new TreeNode(1);
-        TreeNode q = new TreeNode(2);
+        TreeNode p = new TreeNode(0);
+        TreeNode p1 = new TreeNode(1);
+        TreeNode p2 = new TreeNode(2);
+        TreeNode p3 = new TreeNode(3);
+        TreeNode p4 = new TreeNode(4);
+        p1.left = p3;
+        p1.right = p4;
+        p.left = p1;
+        p.right = p2;
+        TreeNode q = new TreeNode(0);
+        TreeNode q1 = new TreeNode(1);
+        TreeNode q2 = new TreeNode(2);
+        TreeNode q3 = new TreeNode(3);
+        TreeNode q4 = new TreeNode(4);
+        q1.left = q3;
+        q1.right = q4;
+        q.left = q1;
+        q.right = q2;
         System.out.println(isSameTree(p, q));
     }
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        return false;
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        else if (p == null || q == null) return false;
+        else {
+            if (p.val == q.val) {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            } else {
+                return false;
+            }
+        }
     }
 
 }
