d618bd248cf7fc53b68a31a86400cdc36c7713aa
Denys <deno4ka@gmail.com>
2017-08-30 10:31:32 -0400

    solved: _107_binary_tree_level_order_traversal_II

--- src/com/deno4ka/LeetCode/Easy/_107_binary_tree_level_order_traversal_II.java
+++ src/com/deno4ka/LeetCode/Easy/_107_binary_tree_level_order_traversal_II.java
@@ -23,13 +23,56 @@ import java.util.List;
 public class _107_binary_tree_level_order_traversal_II {
 
     public _107_binary_tree_level_order_traversal_II() {
-
+//        [1,2,3,3,null,2,null]
+        TreeNode r1 = new TreeNode(3);
+        TreeNode r2_1 = new TreeNode(9);
+        TreeNode r2_2 = new TreeNode(20);
+        TreeNode r3_3 = new TreeNode(15);
+        TreeNode r3_4 = new TreeNode(7);
+        r2_2.left = r3_3;
+        r2_2.right = r3_4;
+        r1.left = r2_1;
+        r1.right = r2_2;
+        System.out.println(levelOrderBottom(r1));
     }
 
     private List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-
+        if (root == null) return result;
+        ArrayList<String> values = new ArrayList<>();
+        int nesting = -1;
+        getSymmetricValues(root, values, nesting);
+//        System.out.println(values);
+        int max = 0;
+        for (String val : values) {
+            String[] valParts = val.split(":");
+            int value = Integer.parseInt(valParts[0]);
+            if (max < value) {
+                max = value;
+            }
+        }
+        int listSize = max;
+        for(int i = 0; i <= listSize; i++) {
+            List<Integer> vals = new ArrayList<>();
+            for(int j = 0; j < values.size(); j++) {
+                String[] valParts = values.get(j).split(":");
+                int value = Integer.parseInt(valParts[0]);
+                if (value == max) {
+                    vals.add(Integer.parseInt(valParts[1]));
+                }
+            }
+            max--;
+            result.add(vals);
+        }
         return result;
+    }
+
+    private void getSymmetricValues(TreeNode tree, ArrayList<String> values, int nesting) {
+        if (tree != null) {
+            getSymmetricValues(tree.left, values, ++nesting);
+            values.add(String.valueOf(nesting) + ":" + String.valueOf(tree.val));
+            getSymmetricValues(tree.right, values, nesting);
+        }
     }
 
     public class TreeNode {
