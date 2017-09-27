1ebf672bb838ffc77e8635033820aac7e83113c9
Denys <deno4ka@gmail.com>
2017-09-27 09:43:08 -0400

    solved: _118_pascals_triangle

--- src/com/deno4ka/LeetCode/Easy/_118_pascals_triangle.java
+++ src/com/deno4ka/LeetCode/Easy/_118_pascals_triangle.java
@@ -19,17 +19,47 @@ import java.util.List;
 public class _118_pascals_triangle {
 
     public _118_pascals_triangle() {
-        System.out.println(generate(0));
-//        System.out.println(generate(1));
-//        System.out.println(generate(2));
+//        print(generate(0));
+//        print(generate(1));
+//        print(generate(2));
+//        print(generate(3));
+//        print(generate(4));
+//        print(generate(5));
+//        print(generate(6));
+//        print(generate(7));
+//        print(generate(8));
+//        print(generate(9));
+//        print(generate(10));
+//        print(generate(11));
+//        print(generate(12));
+        print(generate(13));
     }
 
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
-        if (numRows == 0) {
-            return null;
-        } else {
-            return result;
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    List<Integer> rowBefore = result.get(i - 1);
+                    Integer a = rowBefore.get(j - 1);
+                    Integer b = rowBefore.get(j);
+                    row.add(a + b);
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+
+    private void print(List<List<Integer>> pascalTriangle) {
+        if (pascalTriangle == null) System.out.println("NULL");
+        else {
+            for (List<Integer> row : pascalTriangle) {
+                System.out.println(row);
+            }
         }
     }
 
