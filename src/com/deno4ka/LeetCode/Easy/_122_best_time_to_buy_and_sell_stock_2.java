01ffd8019a5c7f9b0b77c9fffe39089c79696ebc
Denys <deno4ka@gmail.com>
2017-10-03 10:25:37 -0400

    solved: _122_best_time_to_buy_and_sell_stock_2

--- src/com/deno4ka/LeetCode/Easy/_122_best_time_to_buy_and_sell_stock_2.java
+++ src/com/deno4ka/LeetCode/Easy/_122_best_time_to_buy_and_sell_stock_2.java
@@ -10,17 +10,42 @@ However, you may not engage in multiple transactions at the same time (ie, you m
 public class _122_best_time_to_buy_and_sell_stock_2 {
 
     public _122_best_time_to_buy_and_sell_stock_2() {
-        System.out.println(maxProfit(null));
-        System.out.println(maxProfit(new int[] {0}));
+//        System.out.println(maxProfit(null));
+//        System.out.println(maxProfit(new int[] {0}));
+//        System.out.println(maxProfit(new int[] {1, 2}));                // 1
+        System.out.println(maxProfit(new int[] {2,4,1}));                // 2
+//        System.out.println(maxProfit(new int[] {7, 1, 5, 3, 6, 4}));    // 7
     }
 
     private int maxProfit(int[] prices) {
         int maxProfit = 0;
-        if (prices == null) {
+        if (prices == null || prices.length == 0) {
             return maxProfit;
         } else {
-
+            int min = prices[0];
+            int max = Integer.MIN_VALUE;
+            for (int i = 1; i < prices.length; i++) {
+                if (min >= prices[i]) {
+                    if (max > Integer.MIN_VALUE) {
+                        maxProfit += max - min;
+                        max = Integer.MIN_VALUE;
+                    }
+                    min = prices[i];
+                } else {
+                    if (max < prices[i]) {
+                        max = prices[i];
+                    } else {
+                        maxProfit += max - min;
+                        min = prices[i];
+                        max = Integer.MIN_VALUE;
+                    }
+                }
+            }
+            if (max > Integer.MIN_VALUE) {
+                maxProfit += max - min;
+            }
             return maxProfit;
         }
     }
+
 }
