4255764cff321f00a97a2b854ee70c3edb776436
Denys <deno4ka@gmail.com>
2017-10-12 11:24:18 -0400

    inProgress: _125_valid_palindrome

--- src/com/deno4ka/LeetCode/Easy/_125_valid_palindrome.java
+++ src/com/deno4ka/LeetCode/Easy/_125_valid_palindrome.java
@@ -13,8 +13,8 @@ For the purpose of this problem, we define empty string as valid palindrome.
 public class _125_valid_palindrome {
 
     public _125_valid_palindrome() {
-        System.out.println(isPalindrome(null));
-        System.out.println(isPalindrome(""));
+//        System.out.println(isPalindrome(null));
+//        System.out.println(isPalindrome(""));
         System.out.println(isPalindrome("A man, a plan, a canal: Panama")); // true
         System.out.println(isPalindrome("race a car")); // false
     }
@@ -23,7 +23,20 @@ public class _125_valid_palindrome {
         if (s == null || s.isEmpty()) {
             return true;
         } else {
-            return false;
+            char[] charsTmp = s.toLowerCase().toCharArray();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < charsTmp.length; i++) {
+                if (Character.isLetter(charsTmp[i])) {
+                    sb.append(charsTmp[i]);
+                }
+            }
+            char[] letterOnly = sb.toString().toCharArray();
+            for (int j = 0; j < letterOnly.length; j++) {
+                if (letterOnly[j] != letterOnly[letterOnly.length-1-j]) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 
