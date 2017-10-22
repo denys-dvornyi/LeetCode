4f36d456fe83820052d18eb1e17795cfbb9f795f
Denys <deno4ka@gmail.com>
2017-10-22 14:26:23 +0300

    added: _141_linked_list_cycle

---
+++ src/com/deno4ka/LeetCode/Easy/_141_linked_list_cycle.java
@@ -0,0 +1,36 @@
+package com.deno4ka.LeetCode.Easy;
+
+/*
+Given a linked list, determine if it has a cycle in it.
+Follow up:
+Can you solve it without using extra space?
+*/
+
+public class _141_linked_list_cycle {
+
+    public _141_linked_list_cycle() {
+
+    }
+
+    public boolean hasCycle(ListNode head) {
+        boolean result = false;
+        if (head == null) {
+            return result;
+        } else {
+
+        }
+        return result;
+    }
+
+//    Definition for singly-linked list.
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
