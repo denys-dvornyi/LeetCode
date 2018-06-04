a0309b4a749c7dd1b708bd993b05b34f4f206790
Denys <deno4ka@gmail.com>
2018-06-04 03:47:01 -0400

    solved: _167_two_sum_2_input_array_is_sorted

--- src/com/deno4ka/LeetCode/Easy/_167_two_sum_2_input_array_is_sorted.java
+++ src/com/deno4ka/LeetCode/Easy/_167_two_sum_2_input_array_is_sorted.java
@@ -14,8 +14,48 @@ package com.deno4ka.LeetCode.Easy;
 
 public class _167_two_sum_2_input_array_is_sorted {
 
-	public int[] twoSum(int[] numbers, int target) {
-		return null;
+	public _167_two_sum_2_input_array_is_sorted() {
+//		int[] result = twoSum(new int[] {2,3,4}, 6);
+//		int[] result = twoSum(new int[] {2,7,11,15}, 9);
+//		int[] result = twoSum(new int[] {5,25,75}, 100);
+//		System.out.println(result);
 	}
+
+	public int[] twoSum(int[] numbers, int target) {
+		int[] result = new int[2];
+		if (numbers.length == 2) {
+			result[0] = 1;
+			result[1] = 2;
+		} else {
+			for (int i = 0; i < numbers.length - 1; i++) {
+				if (numbers[i] > target) {
+					continue;
+				} else {
+					for (int j = i + 1; j < numbers.length; j++) {
+						if (numbers[i] + numbers[j] == target) {
+							result[0] = i + 1;
+							result[1] = j + 1;
+							break;
+						}
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	// best from leet.code
+//	public int[] twoSum(int[] numbers, int target) {
+//		int x= 0,y=numbers.length-1;
+//		while(x<y) {
+//			//int mid=x+(y-x)/2;
+//			if(numbers[x]+numbers[y]==target)return new int[]{x+1,y+1};
+//			if(numbers[x]+numbers[y]>target)
+//				y--;
+//			else
+//				x++;
+//		}
+//		return numbers[x]+numbers[y]==target?new int[]{x+1,y+1}:new int[]{-1,-1};
+//	}
 
 }
--- src/com/deno4ka/Main.java
+++ src/com/deno4ka/Main.java
@@ -49,7 +49,8 @@ public class Main {
 //        _141_linked_list_cycle linkedListCycle = new _141_linked_list_cycle();
 //        _155_min_stack minStack = new _155_min_stack();
 //        _160_intersection_of_two_linked_lists intersectionOfTwoLinkedLists = new _160_intersection_of_two_linked_lists();
-        _167_two_sum_2_input_array_is_sorted twoSum2InputArrayIsSorted = new _167_two_sum_2_input_array_is_sorted();
+//        _167_two_sum_2_input_array_is_sorted twoSum2InputArrayIsSorted = new _167_two_sum_2_input_array_is_sorted();
+
 
     }
 
