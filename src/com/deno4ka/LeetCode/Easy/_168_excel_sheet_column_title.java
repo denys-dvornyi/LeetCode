32c86a0fdcd43dd4c1ffc613f2872c69597d5376
Denys <deno4ka@gmail.com>
2018-06-06 11:39:26 -0400

    in_progress: _168_excel_sheet_column_title

--- src/com/deno4ka/LeetCode/Easy/_168_excel_sheet_column_title.java
+++ src/com/deno4ka/LeetCode/Easy/_168_excel_sheet_column_title.java
@@ -28,12 +28,60 @@ package com.deno4ka.LeetCode.Easy;
 public class _168_excel_sheet_column_title {
 
 	public _168_excel_sheet_column_title() {
-
+//		System.out.println(convertToTitle(1) + " = A");
+//		System.out.println(convertToTitle(24) + " = X");
+//		System.out.println(convertToTitle(25) + " = Y");
+//		System.out.println(convertToTitle(26) + " = Z");
+//		System.out.println(convertToTitle(27) + " = AA");
+//		System.out.println(convertToTitle(28) + " = AB");
+//		System.out.println(convertToTitle(29) + " = AC");
+//		System.out.println(convertToTitle(50) + " = AX");
+//		System.out.println(convertToTitle(51) + " = AY");
+		System.out.println(convertToTitle(52) + " = AZ");
+//		System.out.println(convertToTitle(53) + " = BA");
+//		System.out.println(convertToTitle(78) + " = BZ");
+//		System.out.println(convertToTitle(79) + " = CA");
+//		System.out.println(convertToTitle(701) + " = ZY");
+//		System.out.println(convertToTitle(702) + " = ZZ");
+//		System.out.println(convertToTitle(703) + " = AAA");
 	}
 
+	// ASCII code A-Z 065-090
 	public String convertToTitle(int n) {
-		String result = "";
-		return result;
+		final int ASCII_CODE_A = 65;
+		final int ASCII_CODE_Z = 90;
+		StringBuilder result = new StringBuilder();
+		if (n < 27) {
+			result.append((char)(ASCII_CODE_A + n - 1));
+		} else {
+			int col = 0;
+			do {
+				int column = n % 26;
+				if (col == 0) {
+					result.insert(0, (char)(ASCII_CODE_A + column - 1));
+					n -= column;
+				} else {
+					result.insert(0, (char)(ASCII_CODE_A + column));
+					n = 26 * column;
+				}
+				col++;
+			} while (n > 0);
+		}
+		return result.toString();
 	}
 
 }
+
+//		if (n < 27) {
+//			result = new char[1];
+//			result[0] = (char)(ASCII_CODE + n);
+//		} else {
+//			result = new char[2];
+//			int column = n % 26;
+//			if (column == 0) {
+//				result[0] = (char) (ASCII_CODE - 1 + (n / 26));
+//				result[1] = (char) (ASCII_CODE + 26);
+//			} else {
+//				result[0] = (char) (ASCII_CODE + (n / 26));
+//				result[1] = (char) (ASCII_CODE + column);
+//			}
