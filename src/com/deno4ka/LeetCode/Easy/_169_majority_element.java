27b5139c6ac56d90cd70bd37991045f920b243fe
Denys <deno4ka@gmail.com>
2018-06-08 07:58:33 -0400

    solved: _169_majority_element

--- src/com/deno4ka/LeetCode/Easy/_169_majority_element.java
+++ src/com/deno4ka/LeetCode/Easy/_169_majority_element.java
@@ -1,5 +1,8 @@
 package com.deno4ka.LeetCode.Easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
 Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 You may assume that the array is non-empty and the majority element always exist in the array.
@@ -16,11 +19,44 @@ Output: 2
 public class _169_majority_element {
 
 	public _169_majority_element() {
-
+		System.out.println(majorityElement(new int[] {3,2,3}) + " - 3");
+		System.out.println(majorityElement(new int[] {2,2,1,1,1,2,2}) + " - 2");
 	}
 
 	public int majorityElement(int[] nums) {
-		return 0;
+		if (nums.length == 1) {
+			return nums[0];
+		} else {
+			Map<Integer, Integer> elements = new HashMap<>();
+			for (int num : nums) {
+				if (elements.containsKey(num)) {
+					elements.put(num, elements.get(num) + 1);
+				} else {
+					elements.put(num, 1);
+				}
+			}
+//			Map.Entry<Integer, Integer> maxElement = null;
+//			for (Map.Entry<Integer, Integer> el : elements.entrySet()) {
+//				if (maxElement == null) {
+//					maxElement = el;
+//				} else {
+//					if (maxElement.getValue().intValue() < el.getValue().intValue()) {
+//						maxElement = el;
+//					}
+//				}
+//			}
+//			return maxElement.getKey().intValue();
+			return elements.entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
+		}
 	}
 
 }
+
+// best from leet.code
+//public int majorityElement(int[] nums) {
+//	if(nums.length == 1) {
+//		return nums[0];
+//	}
+//	Arrays.sort(nums);
+//	return nums[nums.length / 2];
+//}
