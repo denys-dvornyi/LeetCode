ed6fa601f4ecf4dc35c77bbeb66ae6c3488ced78
Denys <deno4ka@gmail.com>
2018-07-13 11:01:07 -0400

    in_progress: _198_house_robber

--- src/com/deno4ka/LeetCode/Easy/_198_house_robber.java
+++ src/com/deno4ka/LeetCode/Easy/_198_house_robber.java
@@ -25,10 +25,127 @@ public class _198_house_robber {
 	public _198_house_robber() {
 		System.out.println(rob(new int[] {1,2,3,1}) + " = 4");
 		System.out.println(rob(new int[] {2,7,9,3,1}) + " = 12");
+		System.out.println(rob(new int[] {2,1,4,13,12}) + " = 18");
+		System.out.println(rob(new int[] {2,1,4,13,3,5,14,1}) + " = 29");
 	}
 
 	public int rob(int[] nums) {
-		return 0;
+		int result = 0;
+		if (nums == null || nums.length == 0) {
+			return result;
+		} else {
+
+			int maxValue = 0;
+			while (maxValue >= 0) {
+				maxValue = -1;
+				int maxValueIndex = 0;
+				for (int i = 0; i < nums.length; i++) {
+					if (nums[i] > 0 && maxValue < nums[i]) {
+						maxValue = nums[i];
+						maxValueIndex = i;
+					}
+				}
+				if (maxValue > 0) {
+					int adjacentSum = 0;
+					nums[maxValueIndex] = -1;
+					if (maxValueIndex > 0) {
+						adjacentSum += nums[maxValueIndex - 1];
+						nums[maxValueIndex - 1] = -1;
+					}
+					if (maxValueIndex + 1 < nums.length) {
+						adjacentSum += nums[maxValueIndex + 1];
+						nums[maxValueIndex + 1] = -1;
+					}
+					if (maxValue >= adjacentSum) {
+						result += maxValue;
+					} else {
+						result += adjacentSum;
+						if (maxValueIndex > 1) {
+							nums[maxValueIndex - 2] = -1;
+						}
+						if (maxValueIndex + 2 < nums.length) {
+							nums[maxValueIndex + 2] = -1;
+						}
+					}
+				}
+			}
+			return result;
+		}
 	}
+
+	// v 2.0 not working for {2,7,9,3,1}
+//	public int rob(int[] nums) {
+//		int result = 0;
+//		if (nums == null || nums.length == 0) {
+//			return result;
+//		} else {
+//			int maxValue = 0;
+//			while (maxValue >= 0) {
+//				maxValue = -1;
+//				int maxValueIndex = 0;
+//				for (int i = 0; i < nums.length; i++) {
+//					if (nums[i] > 0 && maxValue < nums[i]) {
+//						maxValue = nums[i];
+//						maxValueIndex = i;
+//					}
+//				}
+//				if (maxValue > 0) {
+//					int adjacentSum = 0;
+//					nums[maxValueIndex] = -1;
+//					if (maxValueIndex > 0) {
+//						adjacentSum += nums[maxValueIndex - 1];
+//						nums[maxValueIndex - 1] = -1;
+//					}
+//					if (maxValueIndex + 1 < nums.length) {
+//						adjacentSum += nums[maxValueIndex + 1];
+//						nums[maxValueIndex + 1] = -1;
+//					}
+//					if (maxValue >= adjacentSum) {
+//						result += maxValue;
+//					} else {
+//						result += adjacentSum;
+//						if (maxValueIndex > 1) {
+//							nums[maxValueIndex - 2] = -1;
+//						}
+//						if (maxValueIndex + 2 < nums.length) {
+//							nums[maxValueIndex + 2] = -1;
+//						}
+//					}
+//				}
+//			}
+//			return result;
+//		}
+//	}
+
+	// v 1.0 not working for {2,1,4,13,12}
+//	public int rob(int[] nums) {
+//		int result = 0;
+//		if (nums == null || nums.length == 0) {
+//			return result;
+//		} else {
+//			int maxValue = 0;
+//			while (maxValue >= 0) {
+//				maxValue = -1;
+//				int maxValueIndex = 0;
+//				for (int i = 0; i < nums.length; i++) {
+//					if (nums[i] > 0 && maxValue < nums[i]) {
+//						maxValue = nums[i];
+//						maxValueIndex = i;
+//					}
+//				}
+//				if (maxValue > 0) {
+//					result += maxValue;
+//					nums[maxValueIndex] = -1;
+//					if (maxValueIndex > 0) {
+//						nums[maxValueIndex - 1] = -1;
+//					}
+//					if (maxValueIndex + 1 < nums.length) {
+//						nums[maxValueIndex + 1] = -1;
+//					}
+//				}
+//			}
+//			return result;
+//		}
+//	}
 
 }
