fac5055dffd9905bfbb1320f0f39864041b50c3c
deno4ka <Leonid_2012G>
2018-09-13 17:34:04 +0300

    solved : _205_isomorphic_strings (10ms/58.8%)

--- src/com/deno4ka/LeetCode/algorithms/easy/_205_isomorphic_strings.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_205_isomorphic_strings.java
@@ -20,16 +20,45 @@ Note:
 You may assume both s and t have the same length.
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class _205_isomorphic_strings {
 
 	public _205_isomorphic_strings() {
 		System.out.println(isIsomorphic("egg", "add")); // true
 		System.out.println(isIsomorphic("foo", "bar")); // false
 		System.out.println(isIsomorphic("paper", "title")); // true
+		System.out.println(isIsomorphic(null, "not null")); // false
+		System.out.println(isIsomorphic("not null", null)); // false
+		System.out.println(isIsomorphic(null, null)); // false
+		System.out.println(isIsomorphic("long", "very long")); // false
+		System.out.println(isIsomorphic("cool", "good")); // true
+		System.out.println(isIsomorphic("ab", "aa")); // false
 	}
 
 	public boolean isIsomorphic(String s, String t) {
-		return true;
+		if (s == null || t == null || s.length() != t.length()) {
+			return false;
+		} else {
+			Map<Character, Character> characterMap = new HashMap<>();
+			char[] charsS = s.toCharArray();
+			char[] charsT = t.toCharArray();
+			for (int i = 0; i < charsS.length; i++) {
+				if (!characterMap.containsKey(charsS[i])) {
+					if (characterMap.containsValue(charsT[i])) {
+						return false;
+					} else {
+						characterMap.put(charsS[i], charsT[i]);
+					}
+				} else {
+					if (characterMap.get(charsS[i]) != charsT[i]) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
 	}
 
 }
