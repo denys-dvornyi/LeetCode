7aca30d481ec348bc5e335e7cf76e7fce152ab25
deno4ka <Leonid_2012G>
2018-09-14 15:50:40 +0300

    solved : _219_contains_duplicate_2 (557ms/5.77%)

--- src/com/deno4ka/LeetCode/algorithms/easy/_219_contains_duplicate_2.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_219_contains_duplicate_2.java
@@ -16,24 +16,48 @@ Input: nums = [1,2,3,1,2,3], k = 2
 Output: false
 */
 
+import java.util.HashMap;
+
 public class _219_contains_duplicate_2 {
 
 	public _219_contains_duplicate_2() {
-//		System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1}, -1)); // false
-//		System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1}, 0)); // false
-//		System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1}, 4)); // false
+		System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1}, -1)); // false
+		System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1}, 0)); // false
+		System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1}, 4)); // true
 		System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1}, 3)); // true
 		System.out.println(containsNearbyDuplicate(new int[]{1,0,1,1}, 1)); // true
 		System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2)); // false
+		System.out.println(containsNearbyDuplicate(new int[]{99,99}, 2)); // true
+		System.out.println(containsNearbyDuplicate(new int[]{2,2}, 3)); // true
 	}
 
+	// my solution (557ms/5.77%)
 	public boolean containsNearbyDuplicate(int[] nums, int k) {
-		if (nums == null || nums.length == 0 || k < 1 || nums.length <= k) {
+		if (nums == null || nums.length == 0 || k < 1) {
 			return false;
 		} else {
-
-			return true;
+			for (int i = 0; i < nums.length - 1; i++) {
+				for (int j = i + 1; j < nums.length; j++) {
+					if (nums[i] == nums[j] && (j - i) <= k) {
+						return true;
+					}
+				}
+			}
+			return false;
 		}
 	}
+
+	// from leet.code (18ms)
+//	public boolean containsNearbyDuplicate(int[] nums, int k) {
+//		HashMap<Integer,Integer> result = new HashMap<>();
+//		for (int i = 0; i < nums.length; i++) {
+//			if (result.containsKey(nums[i]) && ((i - result.get(nums[i])) <= k)) {
+//				return true;
+//			} else {
+//				result.put(nums[i],i);
+//			}
+//		}
+//		return false;
+//	}
 
 }
