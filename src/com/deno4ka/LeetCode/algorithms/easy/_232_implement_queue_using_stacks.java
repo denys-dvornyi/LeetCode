90c04828f057308ae482f0da3f85da4ad97309d3
deno4ka <Leonid_2012G>
2018-09-18 15:11:46 +0300

    added : _232_implement_queue_using_stacks

---
+++ src/com/deno4ka/LeetCode/algorithms/easy/_232_implement_queue_using_stacks.java
@@ -0,0 +1,52 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+/*
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+
+Example:
+MyQueue queue = new MyQueue();
+queue.push(1);
+queue.push(2);
+queue.peek();  // returns 1
+queue.pop();   // returns 1
+queue.empty(); // returns false
+
+Notes:
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+*/
+
+public class _232_implement_queue_using_stacks {
+
+	/** Initialize your data structure here. */
+	public _232_implement_queue_using_stacks() {
+
+	}
+
+	/** Push element x to the back of queue. */
+	public void push(int x) {
+
+	}
+
+	/** Removes the element from in front of queue and returns that element. */
+	public int pop() {
+		return 0;
+	}
+
+	/** Get the front element. */
+	public int peek() {
+		return 0;
+	}
+
+	/** Returns whether the queue is empty. */
+	public boolean empty() {
+		return false;
+	}
+
+}
