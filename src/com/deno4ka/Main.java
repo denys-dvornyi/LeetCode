c97ef5764025d80d3213de3aa46cdb00f16a6572
deno4ka <Leonid_2012G>
2018-09-20 18:30:13 +0300

    added : _263_ugly_number

--- src/com/deno4ka/Main.java
+++ src/com/deno4ka/Main.java
@@ -65,7 +65,8 @@ public class Main {
 //        _237_delete_node_in_a_linked_list deleteNodeInALinkedList = new _237_delete_node_in_a_linked_list();
 //        _242_valid_anagram validAnagram = new _242_valid_anagram();
 //        _257_binary_tree_paths binaryTreePaths = new _257_binary_tree_paths();
-        _258_add_digits addDigits = new _258_add_digits();
+//        _258_add_digits addDigits = new _258_add_digits();
+        _263_ugly_number uglyNumber = new _263_ugly_number();
 
     }
 
