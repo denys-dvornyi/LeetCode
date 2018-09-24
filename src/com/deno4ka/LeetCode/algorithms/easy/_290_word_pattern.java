6ead7a2d89b22621c2cbd3b282123129516014e1
deno4ka <Leonid_2012G>
2018-09-24 13:10:02 +0300

    solved: _290_word_pattern (1ms/98.33%)

--- src/com/deno4ka/LeetCode/algorithms/easy/_290_word_pattern.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_290_word_pattern.java
@@ -24,6 +24,9 @@ Notes:
 You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class _290_word_pattern {
 
 	public _290_word_pattern() {
@@ -33,8 +36,34 @@ public class _290_word_pattern {
 		System.out.println(wordPattern("abba", "dog dog dog dog")); // false
 	}
 
+	// (1ms/98.33%)
 	public boolean wordPattern(String pattern, String str) {
-		return false;
+		if ((pattern == null || pattern.length() == 0) && (str == null || str.length() == 0)) {
+			return true;
+		} else if ((pattern == null || pattern.length() == 0) && str != null && str.length() > 0) {
+			return false;
+		} else if ((str == null || str.length() == 0) && pattern != null && pattern.length() > 0) {
+			return false;
+		} else {
+			String[] words = str.split(" ");
+			if (words.length != pattern.length()) {
+				return false;
+			} else {
+				Map<Character, String> patternMap = new HashMap<>();
+				for (int i = 0; i < pattern.length(); i++) {
+					if (!patternMap.containsKey(pattern.charAt(i)) && !patternMap.containsValue(words[i])) {
+						patternMap.put(pattern.charAt(i), words[i]);
+					}
+				}
+				for (int j = 0; j < pattern.length(); j++) {
+					String patternWord = patternMap.get(pattern.charAt(j));
+					if (patternWord == null || !patternWord.equals(words[j])) {
+						return false;
+					}
+				}
+				return true;
+			}
+		}
 	}
 
 }
