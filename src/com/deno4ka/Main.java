3b3495b1f4115b1867f5779b6a82da5d69c44259
deno4ka <Leonid_2012G>
2018-09-25 13:03:50 +0300

    added: _342_power_of_four

--- src/com/deno4ka/Main.java
+++ src/com/deno4ka/Main.java
@@ -73,7 +73,8 @@ public class Main {
 //        _290_word_pattern wordPattern = new _290_word_pattern();
 //        _292_nim_game nimGame = new _292_nim_game();
 //        _303_range_sum_query_immutable rangeSumQueryImmutable = new _303_range_sum_query_immutable();
-        _326_power_of_three powerOfThree = new _326_power_of_three();
+//        _326_power_of_three powerOfThree = new _326_power_of_three();
+        _342_power_of_four powerOfFour = new _342_power_of_four();
 
     }
 
