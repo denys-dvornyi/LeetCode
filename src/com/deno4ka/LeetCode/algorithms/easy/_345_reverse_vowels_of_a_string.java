d836efca2ab6c72785a8f081b9311da50e930dbb
deno4ka <Leonid_2012G>
2018-09-25 15:07:49 +0300

    solved: _345_reverse_vowels_of_a_string (4ms/84.30%)

--- src/com/deno4ka/LeetCode/algorithms/easy/_345_reverse_vowels_of_a_string.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_345_reverse_vowels_of_a_string.java
@@ -20,12 +20,30 @@ public class _345_reverse_vowels_of_a_string {
 	public _345_reverse_vowels_of_a_string() {
 		System.out.println(reverseVowels("hello")); // "holle"
 		System.out.println(reverseVowels("leetcode")); // "leotcede"
+		System.out.println(reverseVowels("aA")); // "Aa"
 	}
 
+	// (4ms/84.30%)
 	public String reverseVowels(String s) {
-		if (s == null) return s;
+		if (s == null || s.isEmpty() || s.length() == 1) return s;
+		char[] vowels = new char[s.length()];
+		char[] origChars = s.toCharArray();
+		int vowelCounter = 0;
+		for (int i = 0; i < origChars.length; i++) {
+			if (isVowel(origChars[i])) {
+				vowels[vowelCounter++] = origChars[i];
+			}
+		}
+		for (int j = 0; j < origChars.length; j++) {
+			if (isVowel(origChars[j])) {
+				origChars[j] = vowels[--vowelCounter];
+			}
+		}
+		return new String(origChars);
+	}
 
-		return null;
+	private boolean isVowel(char c) {
+		return c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o'  || c == 'O' || c == 'u' || c == 'U';
 	}
 
 }
