0fbefcf1354c9be002e52905931cd74f3b5e6ce4
deno4ka <Leonid_2012G>
2018-09-25 13:22:48 +0300

    solved: _344_reverse_string (2ms/81.98%)

--- src/com/deno4ka/LeetCode/algorithms/easy/_344_reverse_string.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_344_reverse_string.java
@@ -12,6 +12,8 @@ Input: "A man, a plan, a canal: Panama"
 Output: "amanaP :lanac a ,nalp a ,nam A"
 */
 
+import java.util.Arrays;
+
 public class _344_reverse_string {
 
 	public _344_reverse_string() {
@@ -19,8 +21,18 @@ public class _344_reverse_string {
 		System.out.println(reverseString("A man, a plan, a canal: Panama")); // "amanaP :lanac a ,nalp a ,nam A"
 	}
 
+	// (2ms/81.98%)
 	public String reverseString(String s) {
-		return s;
+		if (s == null || s.isEmpty() || s.length() == 1) {
+			return s;
+		}
+		char[] strAsChars = s.toCharArray();
+		for (int i = 0; i < strAsChars.length / 2; i++) {
+			char tmp = strAsChars[i];
+			strAsChars[i] = strAsChars[strAsChars.length - 1 - i];
+			strAsChars[strAsChars.length - 1 - i] = tmp;
+		}
+		return new String(strAsChars);
 	}
 
 }
