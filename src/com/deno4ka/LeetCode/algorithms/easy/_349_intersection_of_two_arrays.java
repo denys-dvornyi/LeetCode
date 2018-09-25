23910135d0b79e290579f44dee304216b78f33f6
deno4ka <Leonid_2012G>
2018-09-25 16:47:22 +0300

    solved: _349_intersection_of_two_arrays (50ms/5.45%)

--- src/com/deno4ka/LeetCode/algorithms/easy/_349_intersection_of_two_arrays.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_349_intersection_of_two_arrays.java
@@ -17,6 +17,8 @@ The result can be in any order.
 */
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class _349_intersection_of_two_arrays {
 
@@ -25,8 +27,48 @@ public class _349_intersection_of_two_arrays {
 		System.out.println(Arrays.toString(intersection(new int[]{4,9,5}, new int[]{9,4,9,8,4}))); // [9,4]
 	}
 
+	// (50ms/5.45%)
 	public int[] intersection(int[] nums1, int[] nums2) {
-		return null;
+		if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+			return new int[]{};
+		}
+		Set<Integer> set1 = new HashSet<>();
+		Set<Integer> set2 = new HashSet<>();
+		for (int n : nums1) {
+			set1.add(n);
+		}
+		for (int n : nums2) {
+			set2.add(n);
+		}
+		set1.retainAll(set2);
+		return set1.stream().mapToInt(Integer::intValue).toArray();
 	}
+
+	// best from leetcode (3ms)
+//	public int[] intersection(int[] nums1, int[] nums2) {
+//		if (nums1.length > nums2.length) {
+//			int[] temp = nums1;
+//			nums1 = nums2;
+//			nums2 = temp;
+//		}
+//
+//		HashSet<Integer> nums2Set = new HashSet<>();
+//		for (int num : nums2) {
+//			nums2Set.add(num);
+//		}
+//
+//		HashSet<Integer> resSet = new HashSet<>();
+//		for (int num : nums1) {
+//			if (nums2Set.contains(num)) {
+//				resSet.add(num);
+//			}
+//		}
+//		int[] res = new int[resSet.size()];
+//		int count = 0;
+//		for (int num : resSet) {
+//			res[count++] = num;
+//		}
+//		return res;
+//	}
 
 }
