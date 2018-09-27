5ab6cddd56afb94222713dd115a68598f25471c9
deno4ka <Leonid_2012G>
2018-09-27 13:48:58 +0300

    solved: _387_first_unique_character_in_a_string (11ms/93.70%)

--- src/com/deno4ka/LeetCode/algorithms/easy/_387_first_unique_character_in_a_string.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_387_first_unique_character_in_a_string.java
@@ -13,6 +13,10 @@ return 2.
 Note: You may assume the string contain only lowercase letters.
 */
 
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class _387_first_unique_character_in_a_string {
 
 	public _387_first_unique_character_in_a_string() {
@@ -20,8 +24,46 @@ public class _387_first_unique_character_in_a_string {
 		System.out.println(firstUniqChar("loveleetcode")); // 2
 	}
 
+	// with Map slow
+//	public int firstUniqChar(String s) {
+//		if (s == null || s.isEmpty()) {
+//			return -1;
+//		} else if (s.length() == 1) {
+//			return 0;
+//		} else {
+//			Map<Character, Integer> letters = new LinkedHashMap<>();
+//			for (char letter : s.toCharArray()) {
+//				letters.merge(letter, 1, (a, b) -> a + b);
+//			}
+//			for (int i = 0; i < s.length(); i++) {
+//				char letter = s.charAt(i);
+//				if (Integer.valueOf(1).equals(letters.get(letter))) {
+//					return i;
+//				}
+//			}
+//			return -1;
+//		}
+//	}
+
+	// with array (11ms/93.70%)
 	public int firstUniqChar(String s) {
-		return -1;
+		if (s == null || s.isEmpty()) {
+			return -1;
+		} else if (s.length() == 1) {
+			return 0;
+		} else {
+			int[] letters = new int[128];
+			char[] strArr = s.toCharArray();
+			for (char letter : strArr) {
+				letters[letter]++;
+			}
+			for (int i = 0; i < strArr.length; i++) {
+				if (letters[strArr[i]] == 1) {
+					return s.indexOf(strArr[i]);
+				}
+			}
+			return -1;
+		}
 	}
 
 }
