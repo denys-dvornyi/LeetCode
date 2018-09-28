2eb0c626903a5c3017a9bf0719c36bd3da49ee2e
deno4ka <Leonid_2012G>
2018-09-28 17:20:48 +0300

    solved: _401_binary_watch (2ms/88.14%) HARD!

--- src/com/deno4ka/LeetCode/algorithms/easy/_401_binary_watch.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_401_binary_watch.java
@@ -17,17 +17,103 @@ The minute must be consist of two digits and may contain a leading zero, for exa
 */
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class _401_binary_watch {
+
+	private HashMap<Integer, ArrayList<String>> hoursMap;
+	private HashMap<Integer, ArrayList<String>> minutesMap;
 
 	public _401_binary_watch() {
 		System.out.println(readBinaryWatch(1)); // ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
 	}
 
+	// (2ms/88.14%)
 	public List<String> readBinaryWatch(int num) {
 		List<String> binaryWatch = new ArrayList<>();
-		return binaryWatch;
+		if (num < 0 || num > 8) {
+			return binaryWatch;
+		} else {
+			hoursMap = generateHoursMap();
+			minutesMap = generateMinutesMap();
+			int hours = num;
+			int minutes = 0;
+			while (hours >= 0) {
+				if (hours < 4 && minutes < 6) {
+					binaryWatch.addAll(getTime(hours, minutes));
+				}
+				hours--;
+				minutes++;
+			}
+			return binaryWatch;
+		}
+	}
+
+	private ArrayList<String> getTime(int hours, int minutes) {
+		ArrayList<String> time = new ArrayList<>();
+		ArrayList<String> hoursArr = hoursMap.get(hours);
+		ArrayList<String> minutesArr = minutesMap.get(minutes);
+		for (String hour : hoursArr) {
+			for (String minute : minutesArr) {
+				time.add(hour + ":" + minute);
+			}
+		}
+		return time;
+	}
+
+	private HashMap<Integer, ArrayList<String>> generateHoursMap() {
+		HashMap<Integer, ArrayList<String>> hoursMap = new HashMap<>();
+		for (int i = 0; i < 12; i++) {
+			String binaryString = Integer.toBinaryString(i);
+			int bitCounter = 0;
+			int bitIndex = binaryString.indexOf('1');
+			while (bitIndex >= 0) {
+				bitCounter++;
+				bitIndex = binaryString.indexOf('1', bitIndex + 1);
+			}
+			if (!hoursMap.containsKey(bitCounter)) {
+				ArrayList<String> numbers = new ArrayList<>();
+				numbers.add(String.valueOf(i));
+				hoursMap.put(bitCounter, numbers);
+			} else {
+				ArrayList<String> numbers = hoursMap.get(bitCounter);
+				numbers.add(String.valueOf(i));
+				hoursMap.put(bitCounter, numbers);
+			}
+		}
+		return hoursMap;
+	}
+
+	private HashMap<Integer, ArrayList<String>> generateMinutesMap() {
+		HashMap<Integer, ArrayList<String>> minutesMap = new HashMap<>();
+		for (int i = 0; i < 60; i++) {
+			String binaryString = Integer.toBinaryString(i);
+			int bitCounter = 0;
+			int bitIndex = binaryString.indexOf('1');
+			while (bitIndex >= 0) {
+				bitCounter++;
+				bitIndex = binaryString.indexOf('1', bitIndex + 1);
+			}
+			if (!minutesMap.containsKey(bitCounter)) {
+				ArrayList<String> numbers = new ArrayList<>();
+				if (i < 10) {
+					numbers.add("0" + i);
+				} else {
+					numbers.add(String.valueOf(i));
+				}
+				minutesMap.put(bitCounter, numbers);
+			} else {
+				ArrayList<String> numbers = minutesMap.get(bitCounter);
+				if (i < 10) {
+					numbers.add("0" + i);
+				} else {
+					numbers.add(String.valueOf(i));
+				}
+				minutesMap.put(bitCounter, numbers);
+			}
+		}
+		return minutesMap;
 	}
 
 }
