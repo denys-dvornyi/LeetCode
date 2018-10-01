590b15e3b625b00066d33e3976ba3a54e99fc24b
deno4ka <Leonid_2012G>
2018-10-01 17:14:16 +0300

    solved: _412_fizz_buzz (3ms/50.39%)

--- src/com/deno4ka/LeetCode/algorithms/easy/_412_fizz_buzz.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_412_fizz_buzz.java
@@ -20,9 +20,23 @@ public class _412_fizz_buzz {
 		System.out.println(fizzBuzz(15)); // ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
 	}
 
+	// (3ms/50.39%)
 	public List<String> fizzBuzz(int n) {
-		List<String> result = new ArrayList<>();
-
+		List<String> result = new ArrayList<>(n);
+		final String FIZZ = "Fizz"; // %3
+		final String BUZZ = "Buzz"; // %5
+		final String FIZZ_BUZZ = "FizzBuzz"; // %3 && %5
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				result.add(FIZZ_BUZZ);
+			} else if (i % 3 == 0) {
+				result.add(FIZZ);
+			} else if (i % 5 == 0) {
+				result.add(BUZZ);
+			} else {
+				result.add(String.valueOf(i));
+			}
+		}
 		return result;
 	}
 
