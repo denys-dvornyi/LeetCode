ea9526a9dd991160352aa6db4bce17951f17f399
deno4ka <Leonid_2012G>
2018-10-02 18:51:09 +0300

    solved: _429_n_ary_tree_level_order_traversal (4ms/96.46%) NOT EASY!

--- src/com/deno4ka/LeetCode/algorithms/easy/_429_n_ary_tree_level_order_traversal.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_429_n_ary_tree_level_order_traversal.java
@@ -21,15 +21,72 @@ import java.util.List;
 public class _429_n_ary_tree_level_order_traversal {
 
 	public _429_n_ary_tree_level_order_traversal() {
-		System.out.println(levelOrder(null)); //
+//		System.out.println(levelOrder(null)); // []
+
+//		Node grandChildLeft = new Node(5, new ArrayList<>());
+//		Node grandChildRight = new Node(6, new ArrayList<>());
+//		List<Node> grandChildren = new ArrayList<>();
+//		grandChildren.add(grandChildLeft);
+//		grandChildren.add(grandChildRight);
+//		Node childLeft = new Node(3, grandChildren);
+//		Node childMidl = new Node(2, new ArrayList<>());
+//		Node childRight = new Node(4, new ArrayList<>());
+//		List<Node> children = new ArrayList<>();
+//		children.add(childLeft);
+//		children.add(childMidl);
+//		children.add(childRight);
+//		Node root = new Node(1, children);
+//		System.out.println(levelOrder(root)); // [[1],[3,2,4],[5,6]]
+
+		Node grandChildLeft = new Node(6, new ArrayList<>());
+		List<Node> grandChildrenLeft = new ArrayList<>();
+		grandChildrenLeft.add(grandChildLeft);
+		Node childLeft = new Node(3, grandChildrenLeft);
+
+		Node grandChildRight0 = new Node(0, new ArrayList<>());
+		Node grandChildRight5 = new Node(5, new ArrayList<>());
+		List<Node> grandChildrenRight = new ArrayList<>();
+		grandChildrenRight.add(grandChildRight0);
+		grandChildrenRight.add(grandChildRight5);
+		Node childRight = new Node(10, grandChildrenRight);
+
+		List<Node> children = new ArrayList<>();
+		children.add(childLeft);
+		children.add(childRight);
+		Node root = new Node(1, children);
+//		System.out.println(root);
+		System.out.println(levelOrder(root)); // [[1],[10,3],[5,0,6]]
 	}
 
+	// (4ms/96.46%)
 	public List<List<Integer>> levelOrder(Node root) {
 		List<List<Integer>> levelOrder = new ArrayList<>();
 		if (root != null) {
-
+			List<Integer> level = new ArrayList<>();
+			level.add(root.val);
+			levelOrder.add(level);
+			addChildren(root.children, levelOrder, 1);
 		}
 		return levelOrder;
+	}
+
+	private void addChildren(List<Node> children, List<List<Integer>> levelOrder, int depth) {
+		if (children != null && !children.isEmpty()) {
+			List<Integer> childLevel;
+			if (depth >= levelOrder.size()) {
+				childLevel = new ArrayList<>();
+				levelOrder.add(childLevel);
+			} else {
+				childLevel = levelOrder.get(depth);
+				if (childLevel == null) {
+					childLevel = new ArrayList<>();
+				}
+			}
+			for (Node n: children) {
+				childLevel.add(n.val);
+				addChildren(n.children, levelOrder, depth + 1);
+			}
+		}
 	}
 
 	private class Node {
@@ -41,6 +98,14 @@ public class _429_n_ary_tree_level_order_traversal {
 		public Node(int _val, List<Node> _children) {
 			val = _val;
 			children = _children;
+		}
+
+		@Override
+		public String toString() {
+			return "{" +
+					"\"val\":" + val +
+					", \"children\":" + children +
+					'}';
 		}
 	}
 
