21a140a328528d50324eab49f40957d6e6dfeb18
deno4ka <Leonid_2012G>
2018-10-08 15:39:29 +0300

    solved_with_the_help: _437_path_sum_3 (25ms/21.33%) VERY HARD!!!

--- src/com/deno4ka/LeetCode/algorithms/easy/_437_path_sum_3.java
+++ src/com/deno4ka/LeetCode/algorithms/easy/_437_path_sum_3.java
@@ -25,26 +25,25 @@ import java.util.HashMap;
 
 public class _437_path_sum_3 {
 
-
 	public _437_path_sum_3() {
-//		TreeNode root = new TreeNode(10);
-//		TreeNode left = new TreeNode(5);
-//		TreeNode right = new TreeNode(-3);
-//		TreeNode leftLeft = new TreeNode(3);
-//		TreeNode leftRight = new TreeNode(2);
-//		TreeNode rightRight = new TreeNode(11);
-//		TreeNode leftLeftLeft = new TreeNode(3);
-//		TreeNode leftLeftRight = new TreeNode(-2);
-//		TreeNode leftRightRight = new TreeNode(1);
-//		root.left = left;
-//		root.right = right;
-//		right.right = rightRight;
-//		left.left = leftLeft;
-//		left.right = leftRight;
-//		leftRight.right = leftRightRight;
-//		leftLeft.left = leftLeftLeft;
-//		leftLeft.right = leftLeftRight;
-//		System.out.println(pathSum(root, 3)); // 3
+		TreeNode root = new TreeNode(10);
+		TreeNode left = new TreeNode(5);
+		TreeNode right = new TreeNode(-3);
+		TreeNode leftLeft = new TreeNode(3);
+		TreeNode leftRight = new TreeNode(2);
+		TreeNode rightRight = new TreeNode(11);
+		TreeNode leftLeftLeft = new TreeNode(3);
+		TreeNode leftLeftRight = new TreeNode(-2);
+		TreeNode leftRightRight = new TreeNode(1);
+		root.left = left;
+		root.right = right;
+		right.right = rightRight;
+		left.left = leftLeft;
+		left.right = leftRight;
+		leftRight.right = leftRightRight;
+		leftLeft.left = leftLeftLeft;
+		leftLeft.right = leftLeftRight;
+		System.out.println(pathSum(root, 3)); // 3
 //		[10,5,-3,3,2,null,11,3,-2,null,1]
 
 //		System.out.println(pathSum(new TreeNode(1), 1)); // 1
@@ -65,73 +64,155 @@ public class _437_path_sum_3 {
 //		System.out.println(pathSum(root, 3)); // 1
 		//[1,-2,-3,1,3,-2,null,-1]
 
-		TreeNode root = new TreeNode(1);
-		TreeNode left = new TreeNode(2);
-		root.left = left;
-		System.out.println(pathSum(root, 2)); // 1
+//		TreeNode root = new TreeNode(1);
+//		TreeNode left = new TreeNode(2);
+//		root.left = left;
+//		System.out.println(pathSum(root, 2)); // 1
 		//[1,2], 2
+
+//		[0,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,0,null,
+//				0,null,0,null,0,null,0,null,0,null,0,null,0,null,0] 0
 	}
 
+	// from leetcode Prefix sum method. BETTER!!!
+//	public int pathSum(TreeNode root, int sum) {
+//		HashMap<Integer, Integer> preSum = new HashMap();
+//		preSum.put(0,1);
+//		return helper(root, 0, sum, preSum);
+//	}
+//
+//	public int helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+//		if (root == null) {
+//			return 0;
+//		}
+//
+//		currSum += root.val;
+//		int res = preSum.getOrDefault(currSum - target, 0);
+//		preSum.put(currSum, preSum.getOrDefault(currSum, 0) + 1);
+//
+//		res += helper(root.left, currSum, target, preSum) + helper(root.right, currSum, target, preSum);
+//		preSum.put(currSum, preSum.get(currSum) - 1);
+//		return res;
+//	}
+
+	// from leetcode DFS solution. INEFFICIENT!!! (25ms/21.33%)
 	public int pathSum(TreeNode root, int sum) {
-		int result = 0;
-		if (root != null) {
-			ArrayList<ArrayList<Integer>> pathes = new ArrayList<>();
-			getPath(pathes, new ArrayList<>(), root, true);
-			TreeNode rightCursor = root;
-			TreeNode leftCursor = root;
-			while (rightCursor != null || leftCursor != null) {
-				if (rightCursor != null) {
-					rightCursor = rightCursor.right;
-					if (rightCursor != null) {
-						getPath(pathes, new ArrayList<>(), rightCursor, true);
-					}
-				}
-				if (leftCursor != null) {
-					leftCursor = leftCursor.left;
-					if (leftCursor != null) {
-						getPath(pathes, new ArrayList<>(), leftCursor, true);
-					}
-				}
-			}
-//			System.out.println(pathes);
-//			System.out.println(pathes.size());
-			HashMap<Integer, Integer> sumPathes = new HashMap<>();
-			for (ArrayList<Integer> path : pathes) {
-				int sumPath = path.stream().mapToInt(Integer::intValue).sum();
-				if (!sumPathes.containsKey(sumPath)) {
-					sumPathes.put(sumPath, 1);
-				} else {
-					sumPathes.put(sumPath, sumPathes.get(sumPath) + 1);
-				}
-			}
-			if (sumPathes.containsKey(sum)) {
-				result = sumPathes.get(sum);
-			}
-			System.out.println(sumPathes);
-		}
-		return result;
+		if (root == null) return 0;
+		return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
 	}
 
-	private void getPath(ArrayList<ArrayList<Integer>> pathes, ArrayList<Integer> parentPath, TreeNode node, boolean isRoot) {
-		if (node != null) {
-			if (node.left == null && node.right == null && parentPath.size() == 1 && !isRoot) {
-				ArrayList<Integer> leaf = new ArrayList<>();
-				leaf.add(node.val);
-				pathes.add(leaf);
-			}
-
-			parentPath.add(node.val);
-			if (parentPath.size() > 1) {
-				pathes.add(new ArrayList<>(parentPath));
-			}
-			if (isRoot) {
-				pathes.add(new ArrayList<>(parentPath));
-			}
-
-			getPath(pathes, new ArrayList<>(parentPath), node.left, false);
-			getPath(pathes, new ArrayList<>(parentPath), node.right, false);
-		}
+	private int pathSumFrom(TreeNode node, int sum) {
+		if (node == null) return 0;
+		return (node.val == sum ? 1 : 0)
+				+ pathSumFrom(node.left, sum - node.val) + pathSumFrom(node.right, sum - node.val);
 	}
+
+	// my own algorithm. but 'Memory Limit Exceeded' :(
+//	public int pathSum(TreeNode root, int sum) {
+//		int result = 0;
+//		if (root != null) {
+//			ArrayList<ArrayList<Integer>> pathes = new ArrayList<>();
+//			getPath(pathes, new ArrayList<>(), root, true);
+//			if (root.left != null) {
+//				result += pathSum(root.left, sum);
+//			}
+//			if (root.right != null) {
+//				result += pathSum(root.right, sum);
+//			}
+//			HashMap<Integer, Integer> sumPathes = new HashMap<>();
+//			for (ArrayList<Integer> path : pathes) {
+//				int sumPath = path.stream().mapToInt(Integer::intValue).sum();
+//				if (!sumPathes.containsKey(sumPath)) {
+//					sumPathes.put(sumPath, 1);
+//				} else {
+//					sumPathes.put(sumPath, sumPathes.get(sumPath) + 1);
+//				}
+//			}
+//			if (sumPathes.containsKey(sum)) {
+//				result += sumPathes.get(sum);
+//			}
+//		}
+//		return result;
+//	}
+//
+//	private void getPath(ArrayList<ArrayList<Integer>> pathes, ArrayList<Integer> parentPath, TreeNode node, boolean isRoot) {
+//		if (node != null) {
+//			parentPath.add(node.val);
+//			if (parentPath.size() > 1) {
+//				pathes.add(new ArrayList<>(parentPath));
+//			}
+//			if (isRoot) {
+//				pathes.add(new ArrayList<>(parentPath));
+//			}
+//			getPath(pathes, new ArrayList<>(parentPath), node.left, false);
+//			getPath(pathes, new ArrayList<>(parentPath), node.right, false);
+//		}
+//	}
 
 	private class TreeNode {
 		int val;
