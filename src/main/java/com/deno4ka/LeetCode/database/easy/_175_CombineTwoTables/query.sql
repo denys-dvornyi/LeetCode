b431d290e3354f1652c92ac47397ed84ac5ebfd8
deno4ka <deno4ka@gmail.com>
2019-06-01 00:21:44 +0300

    added and solved SQL: _175_CombineTwoTables

---
+++ src/main/java/com/deno4ka/LeetCode/database/easy/_175_CombineTwoTables/query.sql
@@ -0,0 +1,6 @@
+-- TODO: Write a SQL query for a report that provides the following information for each person in the Person table,
+ -- regardless if there is an address for each of those people: FirstName, LastName, City, State
+
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+FROM Person
+         LEFT JOIN Address on Person.PersonId = Address.PersonId;
