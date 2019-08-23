0675ebbec03c70a1e37912221cf043b27e01f0e0
denys.dvornyi <denys.dvornyi@globallobic.com>
2019-08-23 16:46:48 +0300

    in-progress: _500_KeyboardRow

--- src/main/java/com/deno4ka/LeetCode/algorithms/easy/_500_KeyboardRow.java
+++ src/main/java/com/deno4ka/LeetCode/algorithms/easy/_500_KeyboardRow.java
@@ -14,10 +14,33 @@ You may use one character in the keyboard more than once.
 You may assume the input string will only contain letters of alphabet.
 */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class _500_KeyboardRow {
 
 	public String[] findWords(String[] words) {
-		return null;
+		List<Character> firstRow = Arrays.asList('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p');
+		List<Character> secondRow = Arrays.asList('a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l');
+		List<Character> thirdRow = Arrays.asList('z', 'x', 'c', 'v', 'b', 'n', 'm');
+//		char[] firstRow = new char[] {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
+//		char[] secondRow = new char[] {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
+//		char[] thirdRow = new char[] {'z', 'x', 'c', 'v', 'b', 'n', 'm'};
+		List<String> result = new ArrayList<>(words.length);
+		for (int i = 0; i < words.length; i++) {
+			char[] word = words[i].toLowerCase().toCharArray();
+			for (int j = 0; j < word.length; j++) {
+				if (firstRow.containsAll(Arrays.asList(word))) {
+					result.add(word.toString());
+				} else if (secondRow.containsAll(Arrays.asList(word))) {
+					result.add(word.toString());
+				} else if (thirdRow.containsAll(Arrays.asList(word))) {
+					result.add(word.toString());
+				}
+			}
+		}
+		return result.toArray(new String[0]);
 	}
 
 }
