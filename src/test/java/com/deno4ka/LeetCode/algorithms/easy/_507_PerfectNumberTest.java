4284d74b106d9194e4135fe0037a9a2343d88fde
deno4ka <deno4ka@gmail.com>
2019-11-13 15:02:14 -0500

    added: _507_PerfectNumber

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_507_PerfectNumberTest.java
@@ -0,0 +1,32 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class _507_PerfectNumberTest {
+
+	private _507_PerfectNumber perfectNumber = new _507_PerfectNumber();
+
+	@DisplayName("Detect Perfect Number")
+	@ParameterizedTest(name = "test #{index}: perfect number {0} -> expected {1}")
+	@MethodSource({"useCases"})
+	public void fib(int num, boolean expected) {
+		assertEquals(expected, perfectNumber.checkPerfectNumber(num));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(
+						4, false,
+						28, true
+				)
+		);
+	}
+
+}
