f9e984ddd92e061e91cc5d026d6d14d76d1d594f
deno4ka <deno4ka@gmail.com>
2019-11-14 13:57:57 -0500

    added: _520_DetectCapital

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_520_DetectCapitalTest.java
@@ -0,0 +1,32 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _520_DetectCapitalTest {
+
+	private _520_DetectCapital detectCapital = new _520_DetectCapital();
+
+	@DisplayName("Detect Capital")
+	@ParameterizedTest(name = "test #{index}: word {0} -> expected {1}")
+	@MethodSource({"useCases"})
+	public void fib(String word, boolean expected) {
+		Assertions.assertEquals(expected, detectCapital.detectCapitalUse(word));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of("USA", true),
+				Arguments.of("Google", true),
+				Arguments.of("leetcode", true),
+				Arguments.of("FlaG", false),
+				Arguments.of("TrackEnsure", false)
+		);
+	}
+
+}
