9341e5aea67304a4405a97c4ac2c228782e16337
deno4ka <deno4ka@gmail.com>
2019-11-14 13:50:03 -0500

    solved: _504_Base7 (Runtime: 1 ms, faster than 96.47% and Memory Usage: 34.2 MB, less than 100.00%)

--- src/test/java/com/deno4ka/LeetCode/algorithms/easy/_504_Base7Test.java
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_504_Base7Test.java
@@ -1,5 +1,6 @@
 package com.deno4ka.LeetCode.algorithms.easy;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -7,21 +8,21 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+public class _504_Base7Test {
 
-public class _504_Base7 {
-
-	private _506_RelativeRanks relativeRanks = new _506_RelativeRanks();
+	private _504_Base7 base7 = new _504_Base7();
 
 	@DisplayName("return base 7 string representation")
 	@ParameterizedTest(name = "test #{index}: integer {0} -> expected base7 {1}")
 	@MethodSource({"useCases"})
-	public void fib(int[] nums, String[] expected) {
-		assertArrayEquals(expected, relativeRanks.findRelativeRanks(nums));
+	public void fib(int num, String expected) {
+		Assertions.assertEquals(expected, base7.convertToBase7(num));
 	}
 
 	public static Stream<Arguments> useCases() {
 		return Stream.of(
+				Arguments.of(9, "12"),
+				Arguments.of(49, "100"),
 				Arguments.of(100, "202"),
 				Arguments.of(-7, "-10")
 		);
