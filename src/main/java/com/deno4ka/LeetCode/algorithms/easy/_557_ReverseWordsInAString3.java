5b51b7dbbe6c872df9bcb32ebc697355a507d34a
deno4ka <deno4ka@gmail.com>
2019-12-09 14:23:12 -0500

    solved: _557_ReverseWordsInAString3 (Runtime: 3 ms, faster than 95.97% & Memory Usage: 38.3 MB, less than 98.25%)

--- src/main/java/com/deno4ka/LeetCode/algorithms/easy/_557_ReverseWordsInAString3.java
+++ src/main/java/com/deno4ka/LeetCode/algorithms/easy/_557_ReverseWordsInAString3.java
@@ -12,8 +12,34 @@ Note: In the string, each word is separated by single space and there will not b
 
 public class _557_ReverseWordsInAString3 {
 
+	// Runtime: 3 ms, faster than 95.97% & Memory Usage: 38.3 MB, less than 98.25%
 	public String reverseWords(String s) {
-		return s;
+		if (s == null || s.length() < 2) {
+			return s;
+		}
+		char[] string = s.toCharArray();
+		char[] tmp = new char[string.length];
+		int index = 0;
+		int revertIndex = 0;
+		for (int i = 0; i < string.length; i++) {
+			if (string[i] != ' ') {
+				tmp[index] = string[i];
+				index++;
+			} else {
+				while (revertIndex < index) {
+					string[i - index + revertIndex] = tmp[index - 1 - revertIndex];
+					revertIndex++;
+				}
+				index = 0;
+				revertIndex = 0;
+			}
+		}
+		revertIndex = 0;
+		while (revertIndex < index) {
+			string[string.length - index + revertIndex] = tmp[index - 1 - revertIndex];
+			revertIndex++;
+		}
+		return new String(string);
 	}
 
 }
