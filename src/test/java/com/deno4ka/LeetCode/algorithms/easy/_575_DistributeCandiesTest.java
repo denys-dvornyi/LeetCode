79b1238e52b3534a2d4d21f61e56f1fa8c865864
deno4ka <deno4ka@gmail.com>
2019-12-09 14:33:07 -0500

    added: _575_DistributeCandies

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_575_DistributeCandiesTest.java
@@ -0,0 +1,29 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _575_DistributeCandiesTest {
+
+	private _575_DistributeCandies distributeCandies = new _575_DistributeCandies();
+
+	@DisplayName("Distribute candies equally in number to brother and sister")
+	@ParameterizedTest(name = "test #{index}: candies {0} -> expected {2}")
+	@MethodSource({"useCases"})
+	public void distributeCandies(int[] candies, int expected) {
+		Assertions.assertEquals(expected, distributeCandies.distributeCandies(candies));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(new int[] {1, 1, 2, 2, 3, 3}, 3),
+				Arguments.of(new int[] {1, 1, 2, 3}, 2)
+		);
+	}
+
+}
