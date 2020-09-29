7144f9384d254c3e93dd4a02dadf7954e4efc147
deno4ka@gmail.com <Leonid_2012G>
2020-09-29 20:45:59 +0300

    solved: _594_LongestHarmoniousSubsequence (Runtime: 1690 ms, faster than 5.02% & Memory Usage: 39.5 MB, less than 99.90%)

--- src/test/java/com/deno4ka/LeetCode/algorithms/easy/_594_LongestHarmoniousSubsequenceTest.java
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_594_LongestHarmoniousSubsequenceTest.java
@@ -21,7 +21,10 @@ public class _594_LongestHarmoniousSubsequenceTest {
 
 	public static Stream<Arguments> useCases() {
 		return Stream.of(
-				Arguments.of(new int[] {1, 3, 2, 2, 5, 2, 3, 7}, 5)
+				Arguments.of(new int[] {1, 3, 2, 2, 5, 2, 3, 7}, 5),
+				Arguments.of(new int[] {1, 2, 3, 4}, 2),
+				Arguments.of(new int[] {1, 1, 1, 1}, 0),
+				Arguments.of(new int[] {1, 2, 1, 3, 0, 0, 2, 2, 1, 3, 3}, 6)
 		);
 	}
 
