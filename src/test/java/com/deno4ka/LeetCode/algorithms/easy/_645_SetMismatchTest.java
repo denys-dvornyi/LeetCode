89269b23cd2f7ed1907ad3dd570d832c3b1361fc
deno4ka@gmail.com <Leonid_2012G>
2020-10-26 09:53:25 +0200

    added: _645_SetMismatch

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_645_SetMismatchTest.java
@@ -0,0 +1,28 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _645_SetMismatchTest {
+
+	private _645_SetMismatch setMismatch = new _645_SetMismatch();
+
+	@DisplayName("Find the number occurs twice and then find the number that is missing")
+	@ParameterizedTest(name = "test #{index}: array \"{0}\", expected result \"{2}\"")
+	@MethodSource({"useCases"})
+	public void findErrorNums(int[] nums, int[] expected) {
+		Assertions.assertArrayEquals(expected, setMismatch.findErrorNums(nums));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(new int[] {1, 2, 2, 4}, new int[] {2, 3})
+		);
+	}
+
+}
