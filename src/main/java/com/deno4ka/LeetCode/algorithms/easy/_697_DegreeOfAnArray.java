327a2f553c22630f9e18d06ca17096fd5badd9f7
den <deno4ka@gmail.com>
2021-09-20 15:19:36 +0300

    solved: _697_DegreeOfAnArray (Runtime: 45 ms, faster than 14.21% & Memory Usage: 40.8 MB, less than 99.86%)

--- src/main/java/com/deno4ka/LeetCode/algorithms/easy/_697_DegreeOfAnArray.java
+++ src/main/java/com/deno4ka/LeetCode/algorithms/easy/_697_DegreeOfAnArray.java
@@ -25,13 +25,110 @@ nums.length will be between 1 and 50,000.
 nums[i] will be an integer between 0 and 49,999.
 */
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class _697_DegreeOfAnArray {
 
-	public int findShortestSubArray(int[] nums) {
-		if (nums == null || nums.length == 0) return 0;
-		if (nums.length == 1) return 1;
+//	Runtime: 45 ms, faster than 14.21% & Memory Usage: 40.8 MB, less than 99.86%
+//	public int findShortestSubArray(int[] nums) {
+//		if (nums == null || nums.length == 0) return 0;
+//		if (nums.length == 1) return 1;
+//		final int MAX_ELEMENTS = 50_000;
+//		int degree = 0;
+//		int[] numFrequency = new int[MAX_ELEMENTS];
+//		for (int num : nums) {
+//			numFrequency[num]++;
+//			degree = Math.max(degree, numFrequency[num]);
+//		}
+//		int[] mostFrequentNums = new int[MAX_ELEMENTS / degree];
+//		int mostFrequentNumsCursor = 0;
+//		for (int j = 0; j < numFrequency.length; j++) {
+//			if (degree == numFrequency[j]) {
+//				mostFrequentNums[mostFrequentNumsCursor] = j;
+//				mostFrequentNumsCursor++;
+//			}
+//		}
+//		int minSubarrayLength = nums.length;
+//		for (int k = 0; k < mostFrequentNumsCursor; k++) {
+//			int freqNum = mostFrequentNums[k];
+//			int first = -1;
+//			int last = -1;
+//			for (int m = 0; m < nums.length; m++) {
+//				if (freqNum == nums[m]) {
+//					if (first == -1) {
+//						first = m;
+//						last = m;
+//					} else {
+//						last = m;
+//					}
+//				}
+//			}
+//			minSubarrayLength = Math.min(minSubarrayLength, last - first + 1);
+//		}
+//		return minSubarrayLength;
+//	}
 
-		return 0;
+//	fastest from leet.code (2ms)
+//	public int findShortestSubArray(int[] nums) {
+//
+//		int maxVal = 0;
+//		for (int num : nums) {
+//			maxVal = Math.max(num, maxVal);
+//		}
+//		maxVal++;
+//		int[] freq = new int[maxVal];
+//		Integer[] firstNumsIndex = new Integer[maxVal];
+//		int[] lastNumsIndex = new int[maxVal];
+//		int maxFreq = 0;
+//
+//		for(int i=0;i<nums.length;i++)
+//		{
+//			int x=nums[i];
+//			if(firstNumsIndex[x]==null)
+//				firstNumsIndex[x]=i;
+//			maxFreq=Math.max(maxFreq,++freq[x]);
+//			lastNumsIndex[x]=i;
+//		}
+//		int ans=nums.length;
+//		for(int x=0;x<maxVal;x++)
+//		{
+//			if(freq[x]==maxFreq)
+//				ans=Math.min(ans,lastNumsIndex[x]-firstNumsIndex[x]+1);
+//
+//		}
+//		return ans;
+//	}
+
+//  map version from leet.code (17ms)
+	public int findShortestSubArray(int[] nums) {
+		Map<Integer, Integer> freq = new HashMap<>();
+		Map<Integer, Integer> first = new HashMap<>();
+		Map<Integer, Integer> last = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int x = nums[i];
+			if (!first.containsKey(x)) {
+				first.put(x, i);
+			}
+			last.put(x, i);
+			freq.put(x, freq.getOrDefault(x, 0) + 1);
+		}
+		int ans = Integer.MAX_VALUE;
+		int max = Collections.max(freq.values());
+
+		for (int i = 0; i < nums.length; i++) {
+			int x = nums[i];
+			int f = freq.get(x);
+			if (f == max && first.get(x) == i) {
+				int dif = last.get(x) - first.get(x) + 1;
+				if (ans > dif) {
+					ans = dif;
+				}
+			}
+		}
+		return ans;
 	}
 
 }
