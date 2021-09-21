f034e728778ad4990a358b030580e9ad7ea86fd4
den <deno4ka@gmail.com>
2021-09-21 15:45:14 +0300

    added: _728_SelfDividingNumbers

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_728_SelfDividingNumbersTest.java
@@ -0,0 +1,30 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class _728_SelfDividingNumbersTest {
+
+	private final _728_SelfDividingNumbers selfDividingNumbers = new _728_SelfDividingNumbers();
+
+	@DisplayName("Return a list of all the self-dividing numbers in the range [left, right]")
+	@ParameterizedTest(name = "test #{index}: left \"{0}\", right \"{1}\", expected numbers \"{2}\"")
+	@MethodSource({"useCases"})
+	public void selfDividingNumbers(int left, int right, List<Integer> expected) {
+		Assertions.assertEquals(expected, selfDividingNumbers.selfDividingNumbers(left, right));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(1, 22, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22}),
+				Arguments.of(47, 85, new int[]{48, 55, 66, 77})
+		);
+	}
+
+}
