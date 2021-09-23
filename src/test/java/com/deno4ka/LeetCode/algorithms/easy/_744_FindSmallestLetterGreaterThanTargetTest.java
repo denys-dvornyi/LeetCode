1335872ecf40686657d3d6ee4e28d9254a639830
den <deno4ka@gmail.com>
2021-09-23 13:13:01 +0300

    added: _744_FindSmallestLetterGreaterThanTarget

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_744_FindSmallestLetterGreaterThanTargetTest.java
@@ -0,0 +1,33 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _744_FindSmallestLetterGreaterThanTargetTest {
+
+	private final _744_FindSmallestLetterGreaterThanTarget greaterThanTarget = new _744_FindSmallestLetterGreaterThanTarget();
+
+	@DisplayName("Return the smallest character in the array that is larger than target")
+	@ParameterizedTest(name = "test #{index}: letters \"{0}\", target \"{1}\", expected character \"{2}\"")
+	@MethodSource({"useCases"})
+	public void nextGreatestLetter(char[] letters, char target, char expected) {
+		Assertions.assertEquals(expected, greaterThanTarget.nextGreatestLetter(letters, target));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(new char[]{'a', 'b'}, 'z', 'a'),
+				Arguments.of(new char[]{'c', 'f', 'j'}, 'a', 'c'),
+				Arguments.of(new char[]{'c', 'f', 'j'}, 'c', 'f'),
+				Arguments.of(new char[]{'c', 'f', 'j'}, 'd', 'f'),
+				Arguments.of(new char[]{'c', 'f', 'j'}, 'g', 'j'),
+				Arguments.of(new char[]{'c', 'f', 'j'}, 'j', 'c')
+		);
+	}
+
+}
