945f629d18a091884be5825378dc2ae85f96e2ec
den <deno4ka@gmail.com>
2021-10-27 16:53:39 +0300

    added: _747_LargestNumberAtLeastTwiceOfOthers

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_747_LargestNumberAtLeastTwiceOfOthersTest.java
@@ -0,0 +1,30 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _747_LargestNumberAtLeastTwiceOfOthersTest {
+
+	private final _747_LargestNumberAtLeastTwiceOfOthers largestNumberAtLeastTwiceOfOthers = new _747_LargestNumberAtLeastTwiceOfOthers();
+
+	@DisplayName("Return the index of the largest element, or return -1 otherwise")
+	@ParameterizedTest(name = "test #{index}: nums \"{0}\", index \"{1}\"")
+	@MethodSource({"useCases"})
+	public void dominantIndex(int[] nums, int expected) {
+		Assertions.assertEquals(expected, largestNumberAtLeastTwiceOfOthers.dominantIndex(nums));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(new int[]{3, 6, 1, 0}, 1),
+				Arguments.of(new int[]{1, 2, 3, 4}, -1),
+				Arguments.of(new int[]{1}, 0)
+		);
+	}
+
+}
