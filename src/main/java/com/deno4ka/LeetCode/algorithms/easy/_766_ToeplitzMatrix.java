c0839a5a908be2870d7172da6b5cc1be0a1f782a
den <deno4ka@gmail.com>
2021-10-29 16:02:45 +0300

    added: _771_JewelsAndStones

--- src/main/java/com/deno4ka/LeetCode/algorithms/easy/_766_ToeplitzMatrix.java
+++ src/main/java/com/deno4ka/LeetCode/algorithms/easy/_766_ToeplitzMatrix.java
@@ -52,4 +52,18 @@ public class _766_ToeplitzMatrix {
 		return true;
 	}
 
+//  best from leet.code (0ms)
+//	public boolean isToeplitzMatrix(int[][] matrix) {
+//		int m = matrix.length;
+//		int n = matrix[0].length;
+//		for (int i = 1; i < m; i++) {
+//			for (int j = 1; j < n; j++) {
+//				if (matrix[i][j] != matrix[i - 1][j - 1]) {
+//					return false;
+//				}
+//			}
+//		}
+//		return true;
+//	}
+
 }
---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_771_JewelsAndStonesTest.java
@@ -0,0 +1,29 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _771_JewelsAndStonesTest {
+
+	private final _771_JewelsAndStones jewelsAndStones = new _771_JewelsAndStones();
+
+	@DisplayName("Return how many of the stones you have are also jewels")
+	@ParameterizedTest(name = "test #{index}: jewels \"{0}\", stones \"{1}\", jewels in stones \"{2}\"")
+	@MethodSource({"useCases"})
+	public void numJewelsInStones(String jewels, String stones, int expected) {
+		Assertions.assertEquals(expected, jewelsAndStones.numJewelsInStones(jewels, stones));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of("aA", "aAAbbbb", 3),
+				Arguments.of("z", "ZZ", 0)
+		);
+	}
+
+}
