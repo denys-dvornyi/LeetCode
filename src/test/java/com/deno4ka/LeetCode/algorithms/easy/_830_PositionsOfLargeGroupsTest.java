4fba50c08160f2d8fdb10a03b9b8327f932fe710
den <deno4ka@gmail.com>
2021-11-08 17:10:34 +0200

    added: _830_PositionsOfLargeGroups

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_830_PositionsOfLargeGroupsTest.java
@@ -0,0 +1,31 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class _830_PositionsOfLargeGroupsTest {
+
+	private final _830_PositionsOfLargeGroups positionsOfLargeGroups = new _830_PositionsOfLargeGroups();
+
+	@DisplayName("Return the intervals of every large group sorted in increasing order by start index")
+	@ParameterizedTest(name = "test #{index}: letters \"{0}\", expected \"{1}\"")
+	@MethodSource({"useCases"})
+	public void largeGroupPositions(String s, List<List<Integer>> expected) {
+		Assertions.assertEquals(expected, positionsOfLargeGroups.largeGroupPositions(s));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of("abbxxxxzzy", List.of(List.of(3, 6))),
+				Arguments.of("abcdddeeeeaabbbcd", List.of(List.of(3, 5), List.of(6, 9), List.of(12, 14))),
+				Arguments.of("aba", List.of())
+		);
+	}
+
+}
