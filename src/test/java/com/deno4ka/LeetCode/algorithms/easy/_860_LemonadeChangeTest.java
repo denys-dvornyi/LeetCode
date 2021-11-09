2a4230866ad4ac678bc3e18b5352b109c8e79af7
den <deno4ka@gmail.com>
2021-11-09 18:41:39 +0200

    added: _860_LemonadeChange

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_860_LemonadeChangeTest.java
@@ -0,0 +1,31 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _860_LemonadeChangeTest {
+
+	private final _860_LemonadeChange lemonadeChange = new _860_LemonadeChange();
+
+	@DisplayName("Return true if you can provide every customer with correct change, or false otherwise")
+	@ParameterizedTest(name = "test #{index}: bills \"{0}\", expected \"{1}\"")
+	@MethodSource({"useCases"})
+	public void lemonadeChange(int[] bills, boolean expected) {
+		Assertions.assertEquals(expected, lemonadeChange.lemonadeChange(bills));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(new int[]{5, 5, 5, 10, 20}, true),
+				Arguments.of(new int[]{5, 5, 10, 10, 20}, false),
+				Arguments.of(new int[]{5, 5, 10}, true),
+				Arguments.of(new int[]{10, 10}, false)
+		);
+	}
+
+}
