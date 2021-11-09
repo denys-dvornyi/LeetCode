426d3ac3d3bef70f9baeb8bc0107449540f7f9fc
den <deno4ka@gmail.com>
2021-11-09 18:31:44 +0200

    solved: _859_BuddyStrings (Runtime: 1 ms, faster than 98.63% & Memory Usage: 39.1 MB, less than 58.14%)

--- src/main/java/com/deno4ka/LeetCode/algorithms/easy/_859_BuddyStrings.java
+++ src/main/java/com/deno4ka/LeetCode/algorithms/easy/_859_BuddyStrings.java
@@ -25,14 +25,79 @@ Input: s = "aaaaaaabc", goal = "aaaaaaacb"
 Output: true
 
 Constraints:
-1 <= s.length, goal.length <= 2 * 104
+1 <= s.length, goal.length <= 2*10(4)
 s and goal consist of lowercase letters.
 */
 
+import java.util.Locale;
+
 public class _859_BuddyStrings {
 
+//	Runtime: 1 ms, faster than 98.63% & Memory Usage: 39.1 MB, less than 58.14%
 	public boolean buddyStrings(String s, String goal) {
-		return false;
+		if (s.length() != goal.length()) return false;
+		if (s.equals(goal)) {
+			int[] lettersFrequency = new int[26];
+			final int VALUE_OF_A = 'a';
+			for (char c : s.toCharArray()) {
+				int charValue = (int) c - VALUE_OF_A;
+				lettersFrequency[charValue]++;
+				if (lettersFrequency[charValue] > 1) {
+					return true;
+				}
+			}
+			return false;
+		}
+		int mismatchLetters = 0;
+		int[] mismatchIndexes = new int[2];
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != goal.charAt(i)) {
+				if (mismatchLetters == 2) {
+					return false;
+				} else {
+					mismatchIndexes[mismatchLetters++] = i;
+				}
+			}
+		}
+		char[] goalSymbols = goal.toCharArray();
+		// swap mismatch characters
+		char tmp = goalSymbols[mismatchIndexes[1]];
+		goalSymbols[mismatchIndexes[1]] = goalSymbols[mismatchIndexes[0]];
+		goalSymbols[mismatchIndexes[0]] = tmp;
+		return s.equals(new String(goalSymbols));
 	}
+
+//	Time Limit Exceeded
+//	public boolean buddyStrings(String s, String goal) {
+//		int mismatchIndex = -1;
+//		for (int i = 0; i < s.length(); i++) {
+//			if (s.charAt(i) != goal.charAt(i)) {
+//				mismatchIndex = i;
+//				break;
+//			}
+//		}
+//		if (mismatchIndex > 0) {
+//			s = s.substring(mismatchIndex);
+//			goal = goal.substring(mismatchIndex);
+//		}
+//		char[] goalChars = goal.toCharArray();
+//		for (int j = 0; j < goalChars.length; j++) {
+//			for (int k = j + 1; k < goalChars.length; k++) {
+//				swapElements(goalChars, j, k);
+//				if (s.equals(new String(goalChars))) {
+//					return true;
+//				} else {
+//					swapElements(goalChars, j, k);
+//				}
+//			}
+//		}
+//		return false;
+//	}
+
+//	private void swapElements(char[] arrayToSwap, int from, int to) {
+//		char tmp = arrayToSwap[to];
+//		arrayToSwap[to] = arrayToSwap[from];
+//		arrayToSwap[from] = tmp;
+//	}
 
 }
