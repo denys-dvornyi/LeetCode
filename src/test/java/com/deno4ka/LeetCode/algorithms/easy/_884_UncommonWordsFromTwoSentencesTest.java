c2287838636118654899241150618811c72e6647
deno4ka <deno4ka@gmail.com>
2021-11-16 18:03:31 +0200

    added: _884_UncommonWordsFromTwoSentences

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_884_UncommonWordsFromTwoSentencesTest.java
@@ -0,0 +1,29 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _884_UncommonWordsFromTwoSentencesTest {
+
+	private final _884_UncommonWordsFromTwoSentences uncommonWordsFromTwoSentences = new _884_UncommonWordsFromTwoSentences();
+
+	@DisplayName("Return a list of all the uncommon words")
+	@ParameterizedTest(name = "test #{index}: sentence 1 \"{0}\", sentence 2 \"{1}\", uncommon words \"{2}\"")
+	@MethodSource({"useCases"})
+	public void uncommonFromSentences(String s1, String s2, String[] expected) {
+		Assertions.assertEquals(expected, uncommonWordsFromTwoSentences.uncommonFromSentences(s1, s2));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of("this apple is sweet", "this apple is sour", new String[] {"sweet","sour"}),
+				Arguments.of("apple apple", "banana", new String[] {"banana"})
+		);
+	}
+
+}
