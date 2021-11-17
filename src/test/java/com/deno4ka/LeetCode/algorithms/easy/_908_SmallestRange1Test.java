bb217590ccd1d23cffe3ab8436324cce9ae4169e
deno4ka <deno4ka@gmail.com>
2021-11-17 20:05:05 +0200

    added: _908_SmallestRange1

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_908_SmallestRange1Test.java
@@ -0,0 +1,30 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _908_SmallestRange1Test {
+
+	private final _908_SmallestRange1 smallestRange1 = new _908_SmallestRange1();
+
+	@DisplayName("Return the minimum score of nums after applying the mentioned operation at most once for each index in it")
+	@ParameterizedTest(name = "test #{index}: nums \"{0}\", variable \"{1}\", minimum score \"{2}\"")
+	@MethodSource({"useCases"})
+	public void smallestRangeI(int[] nums, int k, int expected) {
+		Assertions.assertEquals(expected, smallestRange1.smallestRangeI(nums, k));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(new int[] {1}, 0, 0),
+				Arguments.of(new int[] {0,10}, 2, 6),
+				Arguments.of(new int[] {1,3,6}, 3, 0)
+		);
+	}
+
+}
