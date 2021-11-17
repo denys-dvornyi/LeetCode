c96b43a39d1470e0b294c92d8683472834b398c5
deno4ka <deno4ka@gmail.com>
2021-11-17 15:33:25 +0200

    added: _905_SortArrayByParity

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_905_SortArrayByParityTest.java
@@ -0,0 +1,30 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _905_SortArrayByParityTest {
+
+	private final _905_SortArrayByParity sortArrayByParity = new _905_SortArrayByParity();
+
+	@DisplayName("Return any array that satisfies this condition")
+	@ParameterizedTest(name = "test #{index}: nums \"{0}\", is monotonic \"{1}\"")
+	@MethodSource({"useCases"})
+	public void sortArrayByParity(int[] nums, int[] expected) {
+		Assertions.assertArrayEquals(expected, sortArrayByParity.sortArrayByParity(nums));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(new int[] {3,1,2,4}, new int[] {2,4,1,3}),
+				Arguments.of(new int[] {0,3,6,1,5,2,9,4}, new int[] {0,6,2,4,9,5,1,3}),
+				Arguments.of(new int[] {0}, new int[] {0})
+		);
+	}
+
+}
