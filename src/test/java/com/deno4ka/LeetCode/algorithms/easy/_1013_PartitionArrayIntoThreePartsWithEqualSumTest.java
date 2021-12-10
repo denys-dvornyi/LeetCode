0f6c1d06bb160c26ef260e2e50376087133e3f2e
deno4ka <deno4ka@gmail.com>
2021-12-10 21:39:38 +0200

    added: _1013_PartitionArrayIntoThreePartsWithEqualSum

---
+++ src/test/java/com/deno4ka/LeetCode/algorithms/easy/_1013_PartitionArrayIntoThreePartsWithEqualSumTest.java
@@ -0,0 +1,30 @@
+package com.deno4ka.LeetCode.algorithms.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class _1013_PartitionArrayIntoThreePartsWithEqualSumTest {
+
+	private final _1013_PartitionArrayIntoThreePartsWithEqualSum partitionArrayIntoThreePartsWithEqualSum = new _1013_PartitionArrayIntoThreePartsWithEqualSum();
+
+	@DisplayName("Return true if we can partition the array into three non-empty parts with equal sums")
+	@ParameterizedTest(name = "test #{index}: array \"{0}\", can be split into three parts \"{1}\"")
+	@MethodSource({"useCases"})
+	public void canThreePartsEqualSum(int[] arr, boolean expected) {
+		Assertions.assertEquals(expected, partitionArrayIntoThreePartsWithEqualSum.canThreePartsEqualSum(arr));
+	}
+
+	public static Stream<Arguments> useCases() {
+		return Stream.of(
+				Arguments.of(new int[]{0,2,1,-6,6,-7,9,1,2,0,1}, true),
+				Arguments.of(new int[]{0,2,1,-6,6,7,9,-1,2,0,1}, false),
+				Arguments.of(new int[]{3,3,6,5,-2,2,5,1,-9,4}, true)
+		);
+	}
+
+}
